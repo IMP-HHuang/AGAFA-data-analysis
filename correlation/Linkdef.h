4f809b3bf9e50f3ad89f271137e62493ca5d3428
H.Huang <45503288+IMP-HHuang@users.noreply.github.com>
2020-08-29 11:28:44 +0800

    Add files via upload

---
+++ correlation/Linkdef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class Event+;
+#pragma link C++ class vector<Event>+;
+#pragma link C++ class vector<Chain> +;
+#pragma link C++ class dssd+;
+#pragma link C++ class vector<dssd>+;
+#endif
